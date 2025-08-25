d2146b84fc08a0c2fcfe28246e9f7d445ff793f9
shin01221 <mohamedmadin372@gmail.com>
2025-08-25 05:08:50 +0300

    end-4 customizations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,81 +1,66 @@
-set -g fish_greeting
-
-source ~/.config/fish/conf.d/hyde.fish
-source ~/.config/fish/user.fish
-
-
-
-if type -q starship
-    starship init fish | source
-    set -gx STARSHIP_CACHE $XDG_CACHE_HOME/starship
-    set -gx STARSHIP_CONFIG $XDG_CONFIG_HOME/starship/starship.toml
+function fish_prompt -d "Write out the prompt"
+    # This shows up as USER@HOST /home/user/ >, with the directory colored
+    # $USER and $hostname are set by fish, so you can just use them
+    # instead of using `whoami` and `hostname`
+    printf '%s@%s %s%s%s > ' $USER $hostname \
+        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
 end
 
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_greeting
 
-if type -q duf
-    function df -d "Run duf with last argument if valid, else run duf"
-        if set -q argv[-1] && test -e $argv[-1]
-            duf $argv[-1]
-        else
-            duf
-        end
-    end
 end
 
-# fzf 
-if type -q fzf
-    fzf --fish | source 
-    for file in ~/.config/fish/functions/fzf/*.fish
-        source $file
-        # NOTE: these funtions are built on top of fzf builtin widgets
-        # they help you navigate through directories and files "Blazingly" fast
-        # to get help on each one, just type `ff` in terminal and press `TAB`
-        # keep in mind all of them require an argument to be passed after the alias
-    end
+starship init fish | source
+if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+    cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
 end
 
+alias pamcan pacman
+alias ls 'eza --icons'
+alias clear "printf '\033[2J\033[3J\033[1;1H'"
+alias q 'qs -c ii'
 
-# NOTE: binds Alt+n to inserting the nth command from history in edit buffer
-# e.g. Alt+4 is same as pressing Up arrow key 4 times
-# really helpful if you get used to it
-bind_M_n_history
+# function fish_prompt
+#   set_color cyan; echo (pwd)
+#   set_color green; echo '> '
+# end
 
+# aliases
 
+# alias down360='down 360'
+# alias down480='down 480'
+# alias down720='down 720'
+# alias down1080='down 1080'
 
-# example integration with bat : <cltr+f>
-# bind -M insert \ce '$EDITOR $(fzf --preview="bat --color=always --plain {}")' 
-
-
-set fish_pager_color_prefix cyan
-set fish_color_autosuggestion brblack 
-
-# List Directory
-alias c='clear'                                                        
-alias l='eza -lh --icons=auto'                                         
-alias ls='eza -1 --icons=auto'                                         
-alias ll='eza -lha --icons=auto --sort=name --group-directories-first' 
-alias ld='eza -lhD --icons=auto'                                       
-alias lt='eza --icons=auto --tree'                                     
-alias un='$aurhelper -Rns'                                             
-alias up='$aurhelper -Syu'                                             
-alias pl='$aurhelper -Qs'                                              
-alias pa='$aurhelper -Ss'                                              
-alias pc='$aurhelper -Sc'                                              
-alias po='$aurhelper -Qtdq | $aurhelper -Rns -'                        
-alias vc='code'                                                        
+alias mirrors='sudo reflector --country Germany,France,Italy --latest 30 --sort age --protocol https --save /etc/pacman.d/mirrorlist'
+alias grep='grep --color=auto'
+alias p='sudo pacman'
+alias rm='trash -d'
+alias cd='z'
+alias cp='cp -r'
+alias cat="bat --theme=base16"
+alias grub-update="sudo grub-mkconfig -o /boot/grub/grub.cfg"
+alias c='clear' # clear terminal
+alias l='eza -lh --icons=auto' # long list
+alias ls='eza -1 --icons=auto' # short list
+alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
+alias ld='eza -lhD --icons=auto' # long list dirs
+alias lt='eza --icons=auto --tree' # list folder as tree
+alias un='$aurhelper -Rns' # uninstall package
+alias up='$aurhelper -Syu' # update system/package/aur
+alias pl='$aurhelper -Qs' # list installed package
+alias pa='$aurhelper -Ss' # list available package
+alias pc='$aurhelper -Sc' # remove unused cache
+alias po='$aurhelper -Qtdq | $aurhelper -Rns -' # remove unused packages, also try > $aurhelper -Qqd | $aurhelper -Rsu --print -
+alias vc='code' # gui code editor
 alias fastfetch='fastfetch --logo-type kitty'
-
-# Directory navigation shortcuts
 alias ..='cd ..'
 alias ...='cd ../..'
 alias .3='cd ../../..'
 alias .4='cd ../../../..'
 alias .5='cd ../../../../..'
+alias mkdir='mkdir -p'
 
-abbr mkdir 'mkdir -p'
-
-
-
-
-
-
+zoxide init fish | source
--- .config/zsh/functions/down.zsh
+++
@@ -1,5 +0,0 @@
-down() {
-  local quality="$1"      # Get the first argument
-  shift                   # Remove the first argument
-  yt-dlp -S "res:${quality}" -- "$@"
-}
