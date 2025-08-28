c1234bfdee291e6b67b3ac8f44f3c38279411b7d
shin01221 <mohamedmadin372@gmail.com>
2025-08-28 18:07:05 +0300

    .

---
+++ .local/bin/hide_unhide_window.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+stack_file="/tmp/hide_window_pid_stack.txt"
+
+hide_window() {
+	pid=$(hyprctl activewindow -j | jq '.pid')
+	hyprctl dispatch movetoworkspacesilent 88,pid:$pid
+	echo $pid >>"$stack_file"
+}
+
+show_window() {
+	pid=$(tail -1 "$stack_file")
+	sed -i '$ d' "$stack_file"
+	[ -z "$pid" ] && exit
+	current=$(hyprctl activeworkspace -j | jq '.id')
+	hyprctl dispatch movetoworkspacesilent "$current",pid:"$pid"
+}
+
+if [[ "$1" == "h" ]]; then
+	hide_window
+else
+	show_window
+fi
