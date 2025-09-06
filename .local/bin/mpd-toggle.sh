16e3d5d140107fd86f91cdd4985f4d2155c17434
shin01221 <mohamedmadin372@gmail.com>
2025-09-06 03:43:38 +0300

    .

--- .local/bin/mpd-toggle.sh
+++ .local/bin/mpd-toggle.sh
@@ -3,9 +3,9 @@
 # Check if mpd is running
 if pgrep -x mpd >/dev/null; then
 	systemctl --user stop mpd.socket mpd.service
-	notify-send mpd is stopped
+	notify-send "mpd is stopped"
 else
 	systemctl --user start mpd.service
 	systemctl --user start mpd-mpris.service
-	notify-send mpd is running
+	notify-send "mpd is running"
 fi
