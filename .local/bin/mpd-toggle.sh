1300eac11b8ba6581bb810f818b6fc235d8f2373
shin01221 <mohamedmadin372@gmail.com>
2025-09-06 03:42:33 +0300

    mpd script fix

--- .local/bin/mpd-toggle.sh
+++ .local/bin/mpd-toggle.sh
@@ -2,10 +2,10 @@
 
 # Check if mpd is running
 if pgrep -x mpd >/dev/null; then
-	systemctl --user stop mpd.service
-	notify-send mpd stopped
+	systemctl --user stop mpd.socket mpd.service
+	notify-send mpd is stopped
 else
 	systemctl --user start mpd.service
 	systemctl --user start mpd-mpris.service
-	notify-send mpd running
+	notify-send mpd is running
 fi
