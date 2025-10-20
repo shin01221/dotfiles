e991b73e1805083a6ce61ab31f5d88f1a00b56e0
shin01221 <mohamedmadin372@gmail.com>
2025-10-21 02:14:59 +0300

    .

--- .config/fish/colors.fish
+++ .config/fish/colors.fish
@@ -1,28 +1,28 @@
-set fish_color_autosuggestion a08c87
+set fish_color_autosuggestion 90909a
 set fish_color_cancel -r
-set fish_color_command ffb5a0
-set fish_color_comment a08c87
-set fish_color_cwd green
+set fish_color_command bbc3ff
+set fish_color_comment 90909a
+set fish_color_cwd bbc3ff
 set fish_color_cwd_root red
-set fish_color_end d9c58d
+set fish_color_end e6bad7
 set fish_color_error ffb4ab
-set fish_color_escape a08c87
+set fish_color_escape 90909a
 set fish_color_history_current --bold
 set fish_color_host normal
 set fish_color_host_remote yellow
-set fish_color_keyword ffb5a0
-set fish_color_match d9c58d
+set fish_color_keyword bbc3ff
+set fish_color_match e6bad7
 set fish_color_normal normal
-set fish_color_operator d9c58d
-set fish_color_option #8f4b38
-set fish_color_param #8f4b38
-set fish_color_quote e7bdb2
-set fish_color_redirection #8f4b38
+set fish_color_operator e6bad7
+set fish_color_option #525a92
+set fish_color_param #525a92
+set fish_color_quote c4c5dd
+set fish_color_redirection #525a92
 
 set fish_color_search_match bryellow '--background=brblack'
 set fish_color_selection white --bold '--background=brblack'
 set fish_color_status red
-set fish_color_user brgreen
+set fish_color_user bbc3ff
 set fish_color_valid_path --underline
 
 set fish_pager_color_background
@@ -30,11 +30,11 @@ set fish_pager_color_completion normal
 set fish_pager_color_description B3A06D yellow
 set fish_pager_color_prefix normal --bold --underline
 set fish_pager_color_progress brwhite '--background=cyan'
-set fish_pager_color_secondary_background
-set fish_pager_color_secondary_completion
-set fish_pager_color_secondary_description
-set fish_pager_color_secondary_prefix
+set fish_pager_color_secondary_background #525a92
+set fish_pager_color_secondary_completion c4c5dd
+set fish_pager_color_secondary_description c4c5dd
+set fish_pager_color_secondary_prefix c4c5dd
 set fish_pager_color_selected_background --background=brblack
-set fish_pager_color_selected_completion
-set fish_pager_color_selected_description
-set fish_pager_color_selected_prefix
+set fish_pager_color_selected_completion c4c5dd
+set fish_pager_color_selected_description c4c5dd
+set fish_pager_color_selected_prefix c4c5dd
--- .config/matugen/templates/fish/fish-colors.fish
+++ .config/matugen/templates/fish/fish-colors.fish
@@ -2,7 +2,7 @@ set fish_color_autosuggestion {{colors.outline.default.hex_stripped}}
 set fish_color_cancel -r
 set fish_color_command {{colors.primary.default.hex_stripped}}
 set fish_color_comment {{colors.outline.default.hex_stripped}}
-set fish_color_cwd green
+set fish_color_cwd {{colors.primary.default.hex_stripped}}
 set fish_color_cwd_root red
 set fish_color_end {{colors.tertiary_fixed_dim.default.hex_stripped}}
 set fish_color_error {{colors.error.default.hex_stripped}}
@@ -22,7 +22,7 @@ set fish_color_redirection {{colors.inverse_primary.default.hex}}
 set fish_color_search_match bryellow '--background=brblack'
 set fish_color_selection white --bold '--background=brblack'
 set fish_color_status red
-set fish_color_user brgreen
+set fish_color_user {{colors.primary.default.hex_stripped}}
 set fish_color_valid_path --underline
 
 set fish_pager_color_background
@@ -30,11 +30,11 @@ set fish_pager_color_completion normal
 set fish_pager_color_description B3A06D yellow
 set fish_pager_color_prefix normal --bold --underline
 set fish_pager_color_progress brwhite '--background=cyan'
-set fish_pager_color_secondary_background
-set fish_pager_color_secondary_completion
-set fish_pager_color_secondary_description
-set fish_pager_color_secondary_prefix
+set fish_pager_color_secondary_background {{colors.inverse_primary.default.hex}}
+set fish_pager_color_secondary_completion {{colors.secondary.default.hex_stripped}}
+set fish_pager_color_secondary_description {{colors.secondary.default.hex_stripped}}
+set fish_pager_color_secondary_prefix {{colors.secondary.default.hex_stripped}}
 set fish_pager_color_selected_background --background=brblack
-set fish_pager_color_selected_completion
-set fish_pager_color_selected_description
-set fish_pager_color_selected_prefix
+set fish_pager_color_selected_completion {{colors.secondary.default.hex_stripped}}
+set fish_pager_color_selected_description {{colors.secondary.default.hex_stripped}}
+set fish_pager_color_selected_prefix {{colors.secondary.default.hex_stripped}}
