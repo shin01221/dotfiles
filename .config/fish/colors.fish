458e5e03b58e04510e55c4df4774007c3bed0b7f
shin01221 <mohamedmadin372@gmail.com>
2025-11-07 02:04:34 +0200

    added some usefull autocmd in nvim

--- .config/fish/colors.fish
+++ .config/fish/colors.fish
@@ -1,82 +1,82 @@
 # Autosuggestions (grey-ish, subtle)
-set fish_color_autosuggestion 53433f
+set fish_color_autosuggestion 52443d
 
 # Ctrl+C feedback
 set fish_color_cancel ffb4ab
 
 # Commands (main accent)
-set fish_color_command ffb59f --bold
+set fish_color_command ffb692 --bold
 
 # Comments
-set fish_color_comment a08c87
+set fish_color_comment a08d85
 
 # Current working directory
-set fish_color_cwd e7bdb2
+set fish_color_cwd e6beac
 set fish_color_cwd_root ffb4ab
 
 # Command separator like `;` or `&&`
-set fish_color_end d8c68d
+set fish_color_end d0c890
 
 # Errors
 set fish_color_error ffb4ab
 
 # Special characters like escapes
-set fish_color_escape 5d4037
+set fish_color_escape 5c4033
 
 # Selected command entry in history search
 set fish_color_history_current --bold
 
 # Host appearance
-set fish_color_host d8c2bc
+set fish_color_host d7c2b9
 set fish_color_host_remote ffb4ab
 
 # Keywords like `if`, `for`
-set fish_color_keyword ffb59f
+set fish_color_keyword ffb692
 
 # Highlight results
-set fish_color_match d8c68d
+set fish_color_match d0c890
 
 # Default text
-set fish_color_normal f1dfda
+set fish_color_normal f0dfd8
 
 # Operators like = + =>
-set fish_color_operator e7bdb2
+set fish_color_operator e6beac
 
 # Options `--flag`
-set fish_color_option e7bdb2
+set fish_color_option e6beac
 
 # Params/arguments
-set fish_color_param d8c68d
+set fish_color_param d0c890
 
 # Strings
-set fish_color_quote e7bdb2
+set fish_color_quote e6beac
 
 # Redirect symbols `>`, `<`, `|`
-set fish_color_redirection 723522
+set fish_color_redirection 703717
 
 # Search highlight colors
-set fish_color_search_match f1dfda --background=322825
+set fish_color_search_match f0dfd8 --background=322824
 
 # Selection in command line
-set fish_color_selection f1dfda --background=322825
+set fish_color_selection f0dfd8 --background=322824
 
 set fish_color_status ffb4ab
-set fish_color_user ffb59f
-set fish_color_valid_path e7bdb2 --underline
+set fish_color_user ffb692
+set fish_color_valid_path e6beac --underline
 
 #######################
 # Pager (tab completion menu)
 #######################
-set fish_pager_color_background 1a110f
-set fish_pager_color_completion d8c2bc
-set fish_pager_color_description a08c87
-set fish_pager_color_prefix ffb59f --bold --underline
-set fish_pager_color_progress e7bdb2
-set fish_pager_color_secondary_background 231917
-set fish_pager_color_secondary_completion f1dfda
-set fish_pager_color_secondary_description 53433f
-set fish_pager_color_secondary_prefix e7bdb2
-set fish_pager_color_selected_background --background=322825
-set fish_pager_color_selected_completion ffb59f
-set fish_pager_color_selected_description f1dfda
-set fish_pager_color_selected_prefix ffb59f
+set fish_pager_color_background 1a120e
+set fish_pager_color_completion d7c2b9
+set fish_pager_color_description a08d85
+set fish_pager_color_prefix ffb692 --bold --underline
+set fish_pager_color_progress e6beac
+set fish_pager_color_secondary_background 221a16
+set fish_pager_color_secondary_completion f0dfd8
+set fish_pager_color_secondary_description 52443d
+set fish_pager_color_secondary_prefix e6beac
+set fish_pager_color_selected_background --background=322824
+set fish_pager_color_selected_completion ffb692
+set fish_pager_color_selected_description f0dfd8
+set fish_pager_color_selected_prefix ffb692
