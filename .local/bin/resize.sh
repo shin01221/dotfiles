7df24ce5587b7442e139dabd2472d3a9ff88f445
shin01221 <mohamedmadin372@gmail.com>
2025-11-07 02:35:21 +0200

    small tweek on resize script

--- .local/bin/resize.sh
+++ .local/bin/resize.sh
@@ -1,5 +1,5 @@
 find "$1" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | while read -r img; do
     height=$(identify -format "%h" "$img")
-    new_height=$((height - 80))
+    new_height=$((height - $2))
     magick.sh --resize-height "$img" "$img" "$new_height"
 done
