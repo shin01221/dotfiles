0880920e8132a476c2a0b610448e6fac9e2106a2
shin01221 <mohamedmadin372@gmail.com>
2025-11-22 19:40:53 +0200

    .

--- .config/fish/colors.fish
+++ .config/fish/colors.fish
@@ -5,7 +5,7 @@ set fish_color_autosuggestion 46464f
 set fish_color_cancel ffb4ab
 
 # Commands (main accent)
-set fish_color_command bbc3ff --bold
+set fish_color_command bac3ff --bold
 
 # Comments
 set fish_color_comment 90909a
@@ -15,7 +15,7 @@ set fish_color_cwd c3c5dd
 set fish_color_cwd_root ffb4ab
 
 # Command separator like `;` or `&&`
-set fish_color_end e6bad7
+set fish_color_end e5bad8
 
 # Errors
 set fish_color_error ffb4ab
@@ -27,14 +27,14 @@ set fish_color_escape 434659
 set fish_color_history_current --bold
 
 # Host appearance
-set fish_color_host c7c5d0
+set fish_color_host c6c5d0
 set fish_color_host_remote ffb4ab
 
 # Keywords like `if`, `for`
-set fish_color_keyword bbc3ff
+set fish_color_keyword bac3ff
 
 # Highlight results
-set fish_color_match e6bad7
+set fish_color_match e5bad8
 
 # Default text
 set fish_color_normal e4e1e9
@@ -46,13 +46,13 @@ set fish_color_operator c3c5dd
 set fish_color_option c3c5dd
 
 # Params/arguments
-set fish_color_param e6bad7
+set fish_color_param e5bad8
 
 # Strings
 set fish_color_quote c3c5dd
 
 # Redirect symbols `>`, `<`, `|`
-set fish_color_redirection 3a4379
+set fish_color_redirection 394379
 
 # Search highlight colors
 set fish_color_search_match e4e1e9 --background=29292f
@@ -61,22 +61,22 @@ set fish_color_search_match e4e1e9 --background=29292f
 set fish_color_selection e4e1e9 --background=29292f
 
 set fish_color_status ffb4ab
-set fish_color_user bbc3ff
+set fish_color_user bac3ff
 set fish_color_valid_path c3c5dd --underline
 
 #######################
 # Pager (tab completion menu)
 #######################
 set fish_pager_color_background 121318
-set fish_pager_color_completion c7c5d0
+set fish_pager_color_completion c6c5d0
 set fish_pager_color_description 90909a
-set fish_pager_color_prefix bbc3ff --bold --underline
+set fish_pager_color_prefix bac3ff --bold --underline
 set fish_pager_color_progress c3c5dd
 set fish_pager_color_secondary_background 1b1b21
 set fish_pager_color_secondary_completion e4e1e9
 set fish_pager_color_secondary_description 46464f
 set fish_pager_color_secondary_prefix c3c5dd
 set fish_pager_color_selected_background --background=29292f
-set fish_pager_color_selected_completion bbc3ff
+set fish_pager_color_selected_completion bac3ff
 set fish_pager_color_selected_description e4e1e9
-set fish_pager_color_selected_prefix bbc3ff
+set fish_pager_color_selected_prefix bac3ff
