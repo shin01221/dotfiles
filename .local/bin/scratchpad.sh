b5ca8248ef93b573b5de8804eca19ff13a64242c
shin01221 <mohamedmadin372@gmail.com>
2026-01-05 15:51:48 +0200

    added toggle behaviour to scratchpad script

--- .local/bin/scratchpad.sh
+++ .local/bin/scratchpad.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
-# kitty --class kitty-dropterm env NO_TMUX=1 fish -c 'tmux attach -t scratch-term 2>/dev/null || tmux new -s scratch-term'
-kitty --class kitty-dropterm env NO_TMUX=1 fish -c 'tmux attach -t scratch-term 2>/dev/null || tmux new -s scratch-term'
+CLASS="$1"
+
+if [ -z "$1" ]; then
+    echo "Error: No class name provided."
+    exit 1
+fi
+
+if pgrep -f "kitty.*--class $CLASS" >/dev/null; then
+    pkill -f "kitty.*--class $CLASS"
+else
+    kitty --class $CLASS env NO_TMUX=1 fish -c \
+        'tmux attach -t scratch-term 2>/dev/null || tmux new -s scratch-term'
+fi
