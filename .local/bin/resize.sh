b8ae8073eb34e600bcb05a7b4127ec1e7b1a40ae
shin01221 <mohamedmadin372@gmail.com>
2026-01-25 12:52:38 +0200

    .

--- .local/bin/resize.sh
+++ .local/bin/resize.sh
@@ -1,3 +1,3 @@
 find "$1" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | while read -r img; do
-    magick.sh --resize-height "$img" "$img" "$2" "$3"
+    magick.sh --resize-height "$img" "$img" "$2"
 done
