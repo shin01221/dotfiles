e5f5e5d6f28c4288a1d89cb7045f4d85be418f45
shin01221 <mohamedmadin372@gmail.com>
2026-02-02 03:00:47 +0200

    added noctalia shell blur

--- .local/bin/ports.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-mapfile -t ports < <(ss -tulpnH | awk '{split($5,a,":"); print $1, a[length(a)]}' | sort | uniq)
-# sudo fuser -n "${ports[$1]}"
-act=$(echo "${ports[@]}" | grep "$1")
-echo "$act"
-# echo "${ports[$1]}"
