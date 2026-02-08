e3641f6b76105793ea5f1513e3b2b05362315382
shin01221 <mohamedmadin372@gmail.com>
2026-02-08 23:34:51 +0200

    .

--- .config/nvim/colors/matugen.lua
+++ .config/nvim/colors/matugen.lua
@@ -1,24 +1,24 @@
 vim.cmd("hi clear")
 require("base16-colorscheme").setup({
   -- Background tones
-  base00 = "#282828", -- Default Background
-  base01 = "#3c3836", -- Lighter Background (status bars)
-  base02 = "#474240", -- Selection Background
-  base03 = "#786f6b", -- Comments, Invisibles
+  base00 = "#070722", -- Default Background
+  base01 = "#11112d", -- Lighter Background (status bars)
+  base02 = "#17173c", -- Selection Background
+  base03 = "#4e4ec2", -- Comments, Invisibles
   -- Foreground tones
-  base04 = "#ebdbb2", -- Dark Foreground (status bars)
-  base05 = "#fbf1c7", -- Default Foreground
-  base06 = "#fbf1c7", -- Light Foreground
-  base07 = "#fbf1c7", -- Lightest Foreground
+  base04 = "#7c80b4", -- Dark Foreground (status bars)
+  base05 = "#f3edf7", -- Default Foreground
+  base06 = "#f3edf7", -- Light Foreground
+  base07 = "#f3edf7", -- Lightest Foreground
   -- Accent colors
-  base08 = "#fb4934", -- Variables, XML Tags, Errors
-  base09 = "#83a598", -- Integers, Constants
-  base0A = "#fabd2f", -- Classes, Search Background
-  base0B = "#b8bb26", -- Strings, Diff Inserted
-  base0C = "#96e9c9", -- Regex, Escape Chars
-  base0D = "#e8e995", -- Functions, Methods
-  base0E = "#fcd782", -- Keywords, Storage
-  --base0F = '#7d0d00', -- Deprecated, Embedded Tags
+  base08 = "#fd4663", -- Variables, XML Tags, Errors
+  base09 = "#9bfece", -- Integers, Constants
+  base0A = "#a9aefe", -- Classes, Search Background
+  base0B = "#fff59b", -- Strings, Diff Inserted
+  base0C = "#81fec1", -- Regex, Escape Chars
+  base0D = "#fff280", -- Functions, Methods
+  base0E = "#8188fe", -- Keywords, Storage
+  --base0F = '#900017', -- Deprecated, Embedded Tags
 })
 
 local signal = vim.uv.new_signal()
