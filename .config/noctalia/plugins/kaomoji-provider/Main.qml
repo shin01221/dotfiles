830aa37c65023b9b842b8e41708281a37e12bb3e
shin01221 <mohamedmadin372@gmail.com>
2026-02-11 19:25:21 +0200

    .

--- .config/noctalia/plugins/kaomoji-provider/Main.qml
+++ .config/noctalia/plugins/kaomoji-provider/Main.qml
@@ -1,6 +1,5 @@
 import QtQuick
 import Quickshell.Io
-import qs.Services.UI
 
 Item {
   property var pluginApi: null
@@ -9,19 +8,7 @@ Item {
     target: "plugin:kaomoji"
     function toggle() {
       pluginApi.withCurrentScreen(screen => {
-        var launcherPanel = PanelService.getPanel("launcherPanel", screen);
-        if (!launcherPanel)
-          return;
-        var searchText = launcherPanel.searchText || "";
-        var isInKaomojiMode = searchText.startsWith(">kaomoji");
-        if (!launcherPanel.isPanelOpen) {
-          launcherPanel.open();
-          launcherPanel.setSearchText(">kaomoji ");
-        } else if (isInKaomojiMode) {
-          launcherPanel.close();
-        } else {
-          launcherPanel.setSearchText(">kaomoji ");
-        }
+        pluginApi.toggleLauncher(screen);
       });
     }
   }
--- .local/bin/workflows.bash
+++ .local/bin/workflows.bash
@@ -2,12 +2,12 @@
 
 workflows_dir="$HOME/.config/hypr/custom/workflows/"
 hypr_config="$HOME/.config/hypr/custom/general.conf"
-fzf_picker=$(command ls ~/.config/hypr/custom/workflows/ | cut -d "." -f 1 | fzf)
+# fzf_picker=$(command ls ~/.config/hypr/custom/workflows/ | cut -d "." -f 1 | fzf)
 illogical_conf="$HOME/.config/illogical-impulse/config.json"
 noctalia_conf="$HOME/.config/noctalia/settings.json"
 foot_conf="$HOME/.config/foot/transparency.conf"
 
-case "$fzf_picker" in
+case "$1" in
 default)
     sed -i 's|source\s*=\s*workflows.*|source=workflows/default.conf|' "$hypr_config"
     sed -i 's/^alpha.*/alpha=.95/' "$foot_conf"
@@ -25,5 +25,8 @@ snappy)
     sed -i 's/^alpha.*/alpha=.95/' "$foot_conf"
     jq -r '.ui.panelBackgroundOpacity = 0.90' "$noctalia_conf" >"$noctalia_conf.tmp" && mv "$noctalia_conf.tmp" "$noctalia_conf"
     jq '.appearance.transparency.enable = true' "$illogical_conf" >"$illogical_conf.tmp" && mv "$illogical_conf.tmp" "$illogical_conf"
+    ;;
+*)
+    echo -e "avillable workspaces is \ndefault\npowersaver\nsnappy "
     ;;
 esac
