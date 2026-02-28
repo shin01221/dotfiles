ab1eb3ad3dc7b0785f28b0d1138cc4ecdbd102d0
shin01221 <mohamedmadin372@gmail.com>
2026-02-28 20:53:12 +0200

    fixed floating dolphin windows (i really hope)

--- .config/nvim/colors/matugen.lua
+++ .config/nvim/colors/matugen.lua
@@ -1,23 +1,23 @@
 vim.cmd("hi clear")
 require("base16-colorscheme").setup({
   -- Background tones
-  base00 = "#141311", -- Default Background
-  base01 = "#211f1d", -- Lighter Background (status bars)
-  base02 = "#2b2a28", -- Selection Background
-  base03 = "#959085", -- Comments, Invisibles
+  base00 = "#181211", -- Default Background
+  base01 = "#251e1d", -- Lighter Background (status bars)
+  base02 = "#2f2827", -- Selection Background
+  base03 = "#a08c89", -- Comments, Invisibles
   -- Foreground tones
-  base04 = "#ccc6ba", -- Dark Foreground (status bars)
-  base05 = "#e6e2de", -- Default Foreground
-  base06 = "#e6e2de", -- Light Foreground
-  base07 = "#e6e2de", -- Lightest Foreground
+  base04 = "#d8c2be", -- Dark Foreground (status bars)
+  base05 = "#ede0dd", -- Default Foreground
+  base06 = "#ede0dd", -- Light Foreground
+  base07 = "#ede0dd", -- Lightest Foreground
   -- Accent colors
   base08 = "#ffb4ab", -- Variables, XML Tags, Errors
-  base09 = "#c0cab6", -- Integers, Constants
-  base0A = "#ccc6b9", -- Classes, Search Background
-  base0B = "#cfc6ac", -- Strings, Diff Inserted
-  base0C = "#c0cab6", -- Regex, Escape Chars
-  base0D = "#cfc6ac", -- Functions, Methods
-  base0E = "#ccc6b9", -- Keywords, Storage
+  base09 = "#dec48c", -- Integers, Constants
+  base0A = "#e7bdb6", -- Classes, Search Background
+  base0B = "#ffb4a7", -- Strings, Diff Inserted
+  base0C = "#dec48c", -- Regex, Escape Chars
+  base0D = "#ffb4a7", -- Functions, Methods
+  base0E = "#e7bdb6", -- Keywords, Storage
   --base0F = '#93000a', -- Deprecated, Embedded Tags
 })
 
