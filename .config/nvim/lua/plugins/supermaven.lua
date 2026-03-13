6288067b28bf6d84d1f156f18ed8397e15f8830d
shin01221 <mohamedmadin372@gmail.com>
2026-03-13 12:13:28 +0200

    fixed weird borders issue

--- .config/nvim/lua/plugins/supermaven.lua
+++ .config/nvim/lua/plugins/supermaven.lua
@@ -1,7 +1,10 @@
 return {
   "supermaven-inc/supermaven-nvim",
   opts = {
-
+    -- disable for certain filetypes
+    -- condition = function()
+    --     return string.match(vim.fn.expand("%:t"), "foo.sh")
+    -- end,
     keymaps = {
       accept_suggestion = "<Tab>",
       clear_suggestion = "<M-]>",
