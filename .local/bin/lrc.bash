36d05c80308b851b1bd45d32d77291dcaa40b6b3
shin01221 <mohamedmadin372@gmail.com>
2026-05-22 11:00:46 +0300

    lyrics only show when either mpd or spotify players playing

--- .local/bin/lrc.bash
+++ .local/bin/lrc.bash
@@ -1,6 +1,9 @@
 players=$(playerctl --list-all | grep -o spotify)
-if [[ $players =~ spotify ]]; then
+statusSpotify=$(playerctl status --player spotify)
+statusMpd=$(playerctl status --player mpd)
+if [[ $players =~ spotify && $statusSpotify =~ Playing ]]; then
     lrc_tty --raw --player spotify
-else
-    lrc_tty --raw
+fi
+if [[ $players =~ mpd && $statusMpd =~ Playing ]]; then
+    lrc_tty --raw --player mpd
 fi
