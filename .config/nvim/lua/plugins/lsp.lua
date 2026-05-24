1bad839f2a225dfbda3dff232d4cec3723d0bc4c
shin01221 <mohamedmadin372@gmail.com>
2026-05-25 01:23:25 +0300

    added snippets for kubernates yaml files

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -11,30 +11,36 @@ return {
     },
   },
   -- vim.lsp.config / vim.lsp.enable calls remain after these plugin specs
-  vim.lsp.config("djls", {
-    cmd = { "djls", "serve" },
-    filetypes = { "htmldjango", "html", "python" },
-    root_markers = { "manage.py", "pyproject.toml", ".git" },
-  }),
-  vim.lsp.config("djlsp", {
-    cmd = { "djlsp" },
-    filetypes = { "htmldjango", "html" },
-    root_markers = { ".git" },
-    settings = {},
-  }),
+  -- vim.lsp.config("djls", {
+  --   cmd = { "djls", "serve" },
+  --   filetypes = { "htmldjango", "html", "python" },
+  --   root_markers = { "manage.py", "pyproject.toml", ".git" },
+  -- }),
+  -- vim.lsp.config("djlsp", {
+  --   cmd = { "djlsp" },
+  --   filetypes = { "htmldjango", "html" },
+  --   root_markers = { ".git" },
+  --   settings = {},
+  -- }),
 
   vim.lsp.config("yamlls", {
-    filetypes = { "yaml", "yaml.docker-compose", "yaml.gitlab", "yaml.helm-values" },
-    schemas = vim.tbl_extend("force", {
-      -- Base schemas from lsp-setup.lua are already included
-      kubernetes = "k8s-*.yaml",
-      ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/**/*.{yml,yaml}",
-      ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-      ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-      ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-      ["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
-    }, {}),
+    settings = {
+      yaml = {
+        filetypes = { "yaml", "yml" },
+        validate = true,
+        completion = true,
+        hover = true,
+        schemas = {
+          -- Base schemas from lsp-setup.lua are already included
+          kubernetes = "k8s-*.yaml",
+          ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/**/*.{yml,yaml}",
+          ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+          ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+          ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+          ["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
+        },
+      },
+    },
   }),
-  vim.lsp.enable("djls"),
-  vim.lsp.enable("djlsp"),
-}
+},
+  vim.lsp.enable("yamlls")
